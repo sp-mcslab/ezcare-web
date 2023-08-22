f5f9ea860b40ba364ad3a11b5df2c511929e3905
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-08-22 15:35:57 +0900

    Remove AuthProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,18 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import React, { useContext } from "react";
-import { AuthProvider } from "@/components/AuthProvider";
 import "../styles/_main.scss";
 import { ThemeContext, ThemeProvider } from "@/context/ThemeContext";
+
 export default function App({ Component, pageProps }: AppProps) {
   const { theme } = useContext(ThemeContext);
   return (
     <ThemeProvider>
-      <AuthProvider>
-        <div className={`${theme}`}>
-          <div className={"background flex"}>
-            <Component {...pageProps} />
-          </div>
+      <div className={`${theme}`}>
+        <div className={"background flex"}>
+          <Component {...pageProps} />
         </div>
-      </AuthProvider>
+      </div>
     </ThemeProvider>
   );
 }

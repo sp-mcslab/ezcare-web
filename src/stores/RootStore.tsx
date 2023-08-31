3c7be5ed2ee624ffa56aa235ab6cd6e9aee26406
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-08-31 22:34:59 +0900

    Delete FriendStore.ts

--- src/stores/RootStore.tsx
+++ src/stores/RootStore.tsx
@@ -1,11 +1,8 @@
 import { RoomListStore } from "@/stores/RoomListStore";
-import { FriendStore } from "@/stores/FriendStore";
 
 export class RootStore {
   roomListStore;
-  friendStore;
   constructor() {
     this.roomListStore = new RoomListStore(this);
-    this.friendStore = new FriendStore(this);
   }
 }
